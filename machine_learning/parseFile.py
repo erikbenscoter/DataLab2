f755deabfe0c97321790cc40b947a3470c35a19e
erikbenscoter <e.benscoter@gmail.com>
2015-04-04 17:39:01 -0400

    added parser of tweet txt file

--- machine_learning/parseFile.py
+++ machine_learning/parseFile.py
@@ -1,4 +1,33 @@
-tweets=[]
-with open("annotated_tweets.txt") as f:
-	tweets = f.readlines()
-print tweets
+import re
+#snowball = python verb processing
+
+def parse():
+	tweets=[]
+	with open("annotated_tweets.txt") as f:
+		tweets = f.readlines()
+
+
+	doubleArray = [];
+	count = 0
+	#will have some number then the code then the tweet
+	for tweet in tweets:
+		#get verdict
+		tokened = re.split('\ ',tweet)
+		tokened.pop(0)
+		groundTruth = tokened.pop(0)
+		if(groundTruth == 'health' or groundTruth=='sick'):
+			tokened.append(1)
+			doubleArray.append(tokened)
+			print("healthy")
+		else:
+			if( groundTruth == 'no'):
+		 		tokened.append(0)
+		 		doubleArray.append(tokened)
+		 		print("sick")
+		 	else:
+		 		#do nothing they are scrap
+		 		count = count + 1
+		 		print("not applicable")
+		 		print count
+	return doubleArray
+parse()
