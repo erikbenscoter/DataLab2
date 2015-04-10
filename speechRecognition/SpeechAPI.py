631773e47ea7886f922810c8e13395ebc1d0532d
dxl5161 <danbardo111@gmail.com>
2015-04-09 23:56:44 -0400

    new file:   speechRecognition/SpeechAPI.py

---
+++ speechRecognition/SpeechAPI.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.WavFile("test.wav") as source:              # use "test.wav" as the audio source
+    r.energy_threshold = 1
+    audio = r.record(source)                        # extract audio data from the file
+
+try:
+    list = r.recognize(audio,True)                  # generate a list of possible transcriptions
+    print("Possible transcriptions:")
+    for prediction in list:
+        print(" " + prediction["text"] + " (" + str(prediction["confidence"]*100) + "%)")
+except LookupError:                                 # speech is unintelligible
+    print("Could not understand audio")
