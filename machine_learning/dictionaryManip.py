b05db4f768979de31e94d56023c89afeaeb450de
dxl5161 <danbardo111@gmail.com>
2015-04-23 00:43:22 -0400

    modified:   dictionaryManip.py

--- machine_learning/dictionaryManip.py
+++ machine_learning/dictionaryManip.py
@@ -1,42 +1,113 @@
+import numpy as np
 import parseFile as ps
-words = dict()
+import bgd
+yes = dict()
+no = dict()
 
 def loadWordsFromFile(filename):
-	tempwords = dict()
+	tempyes = dict()
+	tempno = dict()
 	with open(filename,'r') as f:
 		line = f.readlines()
-		tempwords[line[0]] = line[-1]
-	return tempwords
+		tempyes[line[0]] = line[1]
+		tempno[line[0]] = line[-1]
+	return (tempyes,tempno)
 
-def storeWordsToFile(dictionary,filename):
+def storeWordsToFile(yesDict,noDict,filename):
 	with open(filename,'w') as f:
-		for word in dictionary.keys():
-			#print word
-			#f.write(str(word))
+		for word in yesDict.keys():
 			f.write(str.strip(word))
 			f.write(str(' '))
-			#f.write(str('abc'))
-			f.write(str(dictionary[word]))
+			f.write(str(yesDict[word]))
+			f.write(str(' '))
+			f.write(str(noDict[word]))
 			f.write("\n")
 			
-words = loadWordsFromFile('tester.txt')
+def loadFeatureFromFile(filename):
+	feature = list()
+	(tempyes,tempno) = loadWordsFromFile(filename)
+	for word in tempyes.keys():
+	#FEATURE VECTOR DECISION#
+		if(tempyes[word]/(tempyes[word]+tempno[word])>=.75):
+			feature.append(word)
+	return feature
+	
+(yes,no) = loadWordsFromFile('tester.txt')
 valText = ps.parse()
-#for each tweet
+#for each tweet, manipulate dictionary (part of training)
 for row in valText:
 	#pop V bit from end of row
 	V = row.pop(-1)
 	for word in row:
-		#if the key does not exist in the dictionary, add it with -1/1 weight
-		if words.get(word, 'none') == 'none':
+		#if the key does not exist in the dictionary, add it with 1 weight
+		if yes.get(word, 'none') == 'none':
 			if(V==1):
-				words[word] = 1
+				yes[word] = 1	
+				no[word] = 0
 			else:
-				words[word] = -1
+				yes[word] = 0
+				no[word] = 1
 		else:
 			if(V==1):
-				words[word] += 1
+				yes[word] += 1
 				#print words[word]
 			else:
-				words[word] -= 1
-storeWordsToFile(words,'tester.txt')
-#learn(row,words,V)
+				no[word] += 1
+storeWordsToFile(yes, no,'tester.txt')
+
+valText = ps.parse()
+
+featWrds = loadfeaturefromfile('tester.txt')
+w = numpy.ones(len(x))
+#for each tweet, train/update weights
+for row in valText:
+	#pop V bit from end of row and append it to the target vector
+	V = row.pop(-1)
+	#target.append(V)
+	#convert from words to binary feature vector
+	x = list()
+	for word in featWrds:
+		if word in row:
+			x.append(1)
+		else:
+			x.append(0)
+	#bdg will predict, compare, and possibly update
+	w = bgd(x,V,w,0.5)
+	
+#done training!!!#
+#now test#
+correct = 0
+total = 0
+valText = ps.parse()
+for row in valText:
+	V = row.pop(-1)
+	if (predict(row,w)-V!=0):
+		total+=1
+	else:
+		correct+=1
+		total+=1
+print (float(correct)/total)
+
+
+
+
+
+
+
+
+##SKLEARN'S LOGISTIC REGRESSION##
+target = list()
+input = ps.parse()
+featWrds = loadfeaturefromfile('tester.txt')
+x = list()
+for tweet in input:
+	target.append(tweet.pop(-1));
+	featNum = featWrds
+	for wrd in featNum:
+		if wrd in tweet:
+			wrd = 1
+		else:
+			wrd = 0
+	x.append(featNum)
+model = LogisticRegression(penalty='l2',dual=False,fit_intercept = False,class_weight = 'auto',solver='newton-cg')
+model.fit(x,target)
